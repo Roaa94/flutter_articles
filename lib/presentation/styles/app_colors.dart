9e929de3069de70210ea21690e60a65a97e7850d
roaa94 <roaa94khaddam@gmail.com>
2022-03-21 13:47:39 +0300

    changes on app styles

--- lib/presentation/styles/app_colors.dart
+++ lib/presentation/styles/app_colors.dart
@@ -5,9 +5,13 @@ class AppColors {
 
   static const Color secondary = Color(0xff032730);
 
+  static const Color secondaryLight = Color(0xff032f3b);
+
   static const Color gray = Color(0xffe5e5e5);
 
   static const Color grayLight = Color(0xfff2f2f2);
+
+  static const Color white = Color(0xffffffff);
 
   static Color getShade(Color color, {bool darker = false, double value = .1}) {
     assert(value >= 0 && value <= 1);
--- lib/presentation/styles/app_themes.dart
+++ lib/presentation/styles/app_themes.dart
@@ -2,10 +2,29 @@ import 'package:flutter/material.dart';
 import 'package:flutter_articles/presentation/styles/app_colors.dart';
 
 class AppThemes {
-  static ThemeData get mainTheme {
+  static ThemeData mainTheme({bool isDark = false}) {
     return ThemeData(
+      fontFamily: 'Roboto',
+      brightness: isDark ? Brightness.dark : Brightness.light,
       primaryColor: AppColors.primary,
       primarySwatch: AppColors.getMaterialColorFromColor(AppColors.primary),
+      scaffoldBackgroundColor: isDark ? AppColors.secondary : AppColors.grayLight,
+      cardColor: isDark ? AppColors.secondaryLight : AppColors.white,
+      dividerColor: isDark ? AppColors.white : AppColors.secondary,
+      textTheme: const TextTheme(
+        headline5: TextStyle(fontFamily: 'Roboto', fontWeight: FontWeight.w700),
+      ),
+      appBarTheme: AppBarTheme(
+        backgroundColor: isDark ? AppColors.secondary : AppColors.white,
+        elevation: 0.5,
+        centerTitle: false,
+        toolbarHeight: 70,
+        titleTextStyle: TextStyle(
+          fontSize: 30,
+          color: isDark ? AppColors.white : AppColors.secondary,
+          fontWeight: FontWeight.w700,
+        ),
+      ),
     );
   }
 }
